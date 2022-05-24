7c80473db2e9d2bd62d7a30da5e7e348333c6ed2
ID <izaakduncan@outlook.com>
2022-05-24 04:29:38 -0700

    updated error handling

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let jokeID = document.getElementById('joke-ID')
+let errorDisplay = document.getElementById('errorDisplay')
 
 // Fetch dad joke API
 async function fetchDadJoke() {
     let fetchedDadJoke = await fetch('https://icanhazdadjoke.com/', {
         headers:  {"Accept": "application/json"},
     })
+    if(fetchDadJoke.ok == false)
+    {
+        errorDisplay.hidden == false
+    } else if (errorDisplay.hidden == true) {
+        errorDisplay.hidden == false
+    }
     return await fetchedDadJoke.json()
 }
 
